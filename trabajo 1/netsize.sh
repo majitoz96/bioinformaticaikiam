b6306364fd8249b463fa5ab77bba764fbcd9cb90
majitoz96 <maria.auquilla@est.ikiam.edu.ec>
2021-12-10 21:27:17 -0500

    envio 2
    ejecucion 2

--- trabajo 1/netsize.sh
+++ trabajo 1/netsize.sh
@@ -1,11 +1,17 @@
 
-
+# Tarea n1
+#Maria Jose Auquilla Villamagua 
+#primero elegimos la ubicacion de la carpeta "Saavendra2013" en nuestro computador
+#segundo, dentro de la carpeta creamos un documento en txt.
 cd Saavendra2013
+#tercero, 
 
-cat n1.txt
-
+# Finalmente obtendremos el número de filas y columnas con el siguiente codigo
 echo "El ńumero de columnas es:" ;
 head -n1 n1.txt | grep -o " " | wc -l ;
 echo "El numero de filas es:" ;
-wc -l n1.txt;
+wc -l n1.txt; 
 echo "Fin de la ejecucion"
+# Se obtiene el resultado:
+# El numero de columnas es:80
+# El numero de filas es: 97
--- trabajo 1/netsize_all.sh
+++ trabajo 1/netsize_all.sh
@@ -1,9 +1,13 @@
-
+# Tarea del ejercicio 1.10.3 parte 2
+# Abrir el directoria del archivo Saavendra2013
+# Correr el código:
 cd Saavendra2013
-
-for file in $(ls *.txt);
- do wc -l $file;
- echo "Numero de filas";
- echo "Numero de columnas";
- head -n1 $file | grep -o " " | wc -l;
- done
+for f in $(ls *.txt);
+do
+    echo "El numero de columas es:" ;
+    head -n 1 $f | awk '{print NF}'
+    echo "El numero de filas es:" ;
+    cat $f | wc -l
+done
+echo "Fin de la ejecucion"
+# Se obtienen todos los valores de fila y columna para los 59 archivos .txt
