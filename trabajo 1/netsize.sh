3ded16437636e6973cb319853da136cd9e2fb461
majitoz96 <maria.auquilla@est.ikiam.edu.ec>
2021-12-11 00:44:01 -0500

    Update netsize.sh

--- trabajo 1/netsize.sh
+++ trabajo 1/netsize.sh
@@ -4,14 +4,13 @@
 #primero elegimos la ubicacion de la carpeta "Saavendra2013" en nuestro computador
 #segundo, dentro de la carpeta creamos un documento en txt.
 cd Saavendra2013
-#tercero, 
-
-# Finalmente obtendremos el número de filas y columnas con el siguiente codigo
+#Nota: usualmente tocaria usarle cat pero debido a la cantidad de numeros optare por no hacerlo
+# Tercero, Finalmente aplicamos el codigo 
 echo "El ńumero de columnas es:" ;
 head -n1 n1.txt | grep -o " " | wc -l ;
 echo "El numero de filas es:" ;
 wc -l n1.txt; 
 echo "Fin de la ejecucion"
-# Se obtiene el resultado:
+# Resultados: 
 # El numero de columnas es:80
 # El numero de filas es: 97
