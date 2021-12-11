175b57d722b5c73750ace1ccbd34a93657689c04
majitoz96 <maria.auquilla@est.ikiam.edu.ec>
2021-12-11 00:45:28 -0500

    Update netsize.sh

--- trabajo 1/netsize.sh
+++ trabajo 1/netsize.sh
@@ -5,7 +5,7 @@
 #segundo, dentro de la carpeta creamos un documento en txt.
 cd Saavendra2013
 #Nota: usualmente tocaria usarle cat pero debido a la cantidad de numeros optare por no hacerlo
-# Tercero, Finalmente aplicamos el codigo 
+# Tercero, Finalmente aplicamos el codigo, y en lo personal agrege una leyenda para diferencias el numero de columnas de las filas 
 echo "El ńumero de columnas es:" ;
 head -n1 n1.txt | grep -o " " | wc -l ;
 echo "El numero de filas es:" ;
